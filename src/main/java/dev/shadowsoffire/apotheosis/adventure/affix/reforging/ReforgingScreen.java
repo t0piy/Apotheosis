92697ef763ba96664ee6b05a904dade158eb506f
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2024-05-13 20:06:12 -0700

    Update Reforging UI

--- src/main/java/dev/shadowsoffire/apotheosis/adventure/affix/reforging/ReforgingScreen.java
+++ src/main/java/dev/shadowsoffire/apotheosis/adventure/affix/reforging/ReforgingScreen.java
@@ -1,175 +1,117 @@
 package dev.shadowsoffire.apotheosis.adventure.affix.reforging;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 
 import dev.shadowsoffire.apotheosis.Apotheosis;
 import dev.shadowsoffire.apotheosis.adventure.Adventure.Items;
-import dev.shadowsoffire.apotheosis.adventure.loot.LootController;
-import dev.shadowsoffire.apotheosis.adventure.loot.LootRarity;
-import dev.shadowsoffire.apotheosis.util.DrawsOnLeft;
+import dev.shadowsoffire.apotheosis.adventure.affix.reforging.ReforgingMenu.ReforgingResultSlot;
+import dev.shadowsoffire.apotheosis.adventure.affix.salvaging.SalvagingScreen;
+import dev.shadowsoffire.apotheosis.adventure.client.AdventureContainerScreen;
+import dev.shadowsoffire.apotheosis.adventure.client.GhostVertexBuilder;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.client.gui.screens.inventory.EnchantmentNames;
-import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.FormattedText;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.RandomSource;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.registries.ForgeRegistries;
 
-public class ReforgingScreen extends AbstractContainerScreen<ReforgingMenu> implements DrawsOnLeft {
+public class ReforgingScreen extends AdventureContainerScreen<ReforgingMenu> {
 
+    /**
+     * This texture is 256x384, which must be reflected in blit() calls.
+     */
     public static final ResourceLocation TEXTURE = new ResourceLocation(Apotheosis.MODID, "textures/gui/reforge.png");
+    public static final ResourceLocation ANIMATED_TEXTURE = new ResourceLocation(Apotheosis.MODID, "textures/gui/reforge_animation.png");
 
-    protected ItemStack[] choices = new ItemStack[3];
-    protected ItemStack lastInput = ItemStack.EMPTY;
-    protected LootRarity lastRarity = null;
-    protected Component title;
+    protected boolean hasMainItem = false;
+    protected int animationTick = 0;
+    protected int maxSlot = -1;
+
+    protected int availableOpacity = 0xAA;
 
     public ReforgingScreen(ReforgingMenu menu, Inventory inv, Component title) {
         super(menu, inv, title);
-        this.titleLabelY = 5;
-        Arrays.fill(this.choices, ItemStack.EMPTY);
-        this.title = Component.translatable("container.apotheosis.reforge");
-    }
-
-    public boolean shouldRecompute() {
-        ItemStack input = this.menu.getSlot(0).getItem();
-        LootRarity rarity = this.getMenu().getRarity();
-        return !ItemStack.isSameItemSameTags(input, this.lastInput) || this.lastRarity != rarity;
-    }
-
-    public void recomputeChoices() {
-        ItemStack input = this.menu.getSlot(0).getItem();
-        LootRarity rarity = this.getMenu().getRarity();
-        if (input.isEmpty() || rarity == null) {
-            Arrays.fill(this.choices, ItemStack.EMPTY);
-        }
-        else {
-            RandomSource rand = this.menu.random;
-            for (int i = 0; i < 3; i++) {
-                rand.setSeed(this.menu.getSeed() ^ ForgeRegistries.ITEMS.getKey(input.getItem()).hashCode() + i);
-                this.choices[i] = LootController.createLootItem(input.copy(), rarity, rand);
-            }
-        }
-        this.lastInput = input.copy();
-        this.lastRarity = rarity;
+        this.imageHeight = 266;
     }
 
     @Override
-    public void render(GuiGraphics gfx, int x, int y, float pPartialTick) {
-        if (this.shouldRecompute()) this.recomputeChoices();
+    public void render(GuiGraphics gfx, int mouseX, int mouseY, float pPartialTick) {
         this.renderBackground(gfx);
-        super.render(gfx, x, y, pPartialTick);
+        super.render(gfx, mouseX, mouseY, pPartialTick);
         RenderSystem.disableBlend();
-        this.renderTooltip(gfx, x, y);
-
-        int xCenter = (this.width - this.imageWidth) / 2;
-        int yCenter = (this.height - this.imageHeight) / 2;
+        this.renderTooltip(gfx, mouseX, mouseY);
 
         int dust = this.menu.getDustCount();
         int mats = this.menu.getMatCount();
         int levels = this.menu.player.experienceLevel;
 
-        for (int slot = 0; slot < 3; ++slot) {
-            ItemStack choice = this.choices[slot];
-            if (choice.isEmpty() || this.menu.needsReset()) continue;
-            List<Component> tooltips = new ArrayList<>();
+        for (int idx = 0; idx < 3; ++idx) {
+            Slot slot = this.getMenu().getSlot(3 + idx);
+            if (this.isHovering(slot.x, slot.y, 16, 16, mouseX, mouseY)) {
+                ItemStack choice = slot.getItem();
+                if (choice.isEmpty()) continue;
+                List<Component> tooltips = new ArrayList<>();
 
-            int dustCost = this.menu.getDustCost(slot);
-            int matCost = this.menu.getMatCost(slot);
-            int levelCost = this.menu.getLevelCost(slot);
+                int dustCost = this.menu.getDustCost(idx);
+                int matCost = this.menu.getMatCost(idx);
+                int levelCost = this.menu.getLevelCost(idx);
+                boolean creative = this.minecraft.player.isCreative();
 
-            tooltips.add(Component.translatable("text.apotheosis.reforge_cost").withStyle(ChatFormatting.YELLOW, ChatFormatting.UNDERLINE));
-            tooltips.add(CommonComponents.EMPTY);
-            if (dustCost > 0) {
-                tooltips.add(Component.translatable("%s %s", dustCost, Items.GEM_DUST.get().getName(ItemStack.EMPTY)).withStyle(dust < dustCost ? ChatFormatting.RED : ChatFormatting.GRAY));
-            }
-            if (matCost > 0) {
-                tooltips.add(Component.translatable("%s %s", matCost, this.menu.getSlot(1).getItem().getHoverName()).withStyle(mats < matCost ? ChatFormatting.RED : ChatFormatting.GRAY));
-            }
-            String key = levels >= levelCost ? levelCost == 1 ? "container.enchant.level.one" : "container.enchant.level.many" : "container.enchant.level.requirement";
+                tooltips.add(Component.translatable("text.apotheosis.reforge_cost").withStyle(ChatFormatting.YELLOW, ChatFormatting.UNDERLINE));
+                tooltips.add(CommonComponents.EMPTY);
+                if (dustCost > 0) {
+                    tooltips.add(Component.translatable("%s %s", dustCost, Items.GEM_DUST.get().getName(ItemStack.EMPTY)).withStyle(creative || dust >= dustCost ? ChatFormatting.GRAY : ChatFormatting.RED));
+                }
+                if (matCost > 0) {
+                    tooltips.add(Component.translatable("%s %s", matCost, this.menu.getSlot(1).getItem().getHoverName().getString()).withStyle(creative || mats >= matCost ? ChatFormatting.GRAY : ChatFormatting.RED));
+                }
+                String key = idx == 0 ? "container.enchant.level.one" : "container.enchant.level.many";
+                tooltips.add(Component.translatable(key, idx + 1).withStyle(creative || levels >= levelCost ? ChatFormatting.GRAY : ChatFormatting.RED));
 
-            tooltips.add(Component.translatable(key, levelCost).withStyle(levels < levelCost ? ChatFormatting.RED : ChatFormatting.GRAY));
+                tooltips.add(Component.literal(" "));
+                tooltips.add(Component.translatable("container.enchant.level.requirement", levelCost).withStyle(creative || levels >= levelCost ? ChatFormatting.GRAY : ChatFormatting.RED));
 
-            int k2 = x - (xCenter + 60);
-            int l2 = y - (yCenter + 14 + 19 * slot);
-            if (k2 >= 0 && l2 >= 0 && k2 < 108 && l2 < 19) {
-                gfx.renderTooltip(this.font, choice, x, y);
-                this.drawOnLeft(gfx, tooltips, this.getGuiTop() + 29);
+                this.drawOnLeft(gfx, tooltips, this.getGuiTop() + 45);
+                break;
             }
         }
     }
 
     @Override
     protected void renderBg(GuiGraphics gfx, float partials, int x, int y) {
+        int left = this.getGuiLeft();
+        int top = this.getGuiTop();
         int xCenter = (this.width - this.imageWidth) / 2;
-        int slotsX = xCenter + 60;
         int yCenter = (this.height - this.imageHeight) / 2;
-        gfx.blit(TEXTURE, xCenter, yCenter, 0, 0, this.imageWidth, this.imageHeight);
+        gfx.blit(TEXTURE, xCenter, yCenter, 0, 0, this.imageWidth, this.imageHeight, 256, 384);
 
-        LootRarity rarity = this.menu.getRarity();
-
-        if (this.menu.getSlot(0).getItem().isEmpty() || rarity == null || this.menu.needsReset()) {
-            for (int slot = 0; slot < 3; ++slot) {
-                gfx.blit(TEXTURE, slotsX, yCenter + 14 + 19 * slot, 0, 166 + 19, 108, 19);
+        for (int idx = 0; idx < 3; idx++) {
+            if (this.maxSlot >= idx && this.animationTick == 0) {
+                gfx.blit(TEXTURE, left + 20 + 46 * idx, top + 129, 20 + 46 * idx, 273, 46, 35, 256, 384);
             }
-            return;
         }
 
-        int dust = this.menu.getDustCount();
-        int mats = this.menu.getMatCount();
-        int levels = this.menu.player.experienceLevel;
+        boolean hadItem = this.hasMainItem;
+        this.hasMainItem = this.menu.getSlot(0).hasItem();
 
-        EnchantmentNames.getInstance().initSeed(this.menu.getSeed());
-        for (int slot = 0; slot < 3; ++slot) {
-            RenderSystem.setShader(GameRenderer::getPositionTexShader);
-            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-            RenderSystem.setShaderTexture(0, TEXTURE);
+        final int FRAME_TIME = 20;
 
-            int dustCost = this.menu.getDustCost(slot);
-            int matCost = this.menu.getMatCost(slot);
-            int levelCost = this.menu.getLevelCost(slot);
-            int maxCost = this.menu.getMatCost(2);
+        if (!hadItem && hasMainItem) {
+            this.animationTick = FRAME_TIME;
+        }
 
-            String levelStr = "" + levelCost;
-            String costStr = "" + matCost;
-            int width = 86 - this.font.width(levelStr + maxCost);
-            int randTextX = slotsX + 15 + this.font.width("" + maxCost);
-            FormattedText randText = EnchantmentNames.getInstance().getRandomName(this.font, width);
-            int color = 0x515151;
-            if ((dust < dustCost || levels < levelCost || mats < matCost) && !this.minecraft.player.getAbilities().instabuild) {
-                gfx.blit(TEXTURE, slotsX, yCenter + 14 + 19 * slot, 0, 166 + 19, 108, 19);
-                gfx.blit(TEXTURE, slotsX + 1, yCenter + 15 + 19 * slot, 16 * slot, 239, 16, 16);
-                gfx.drawWordWrap(this.font, randText, randTextX, yCenter + 16 + 19 * slot, width, color);
-                color = this.darken(rarity.getColor().getValue(), 2);
-            }
-            else {
-                int k2 = x - (xCenter + 60);
-                int l2 = y - (yCenter + 14 + 19 * slot);
-                if (k2 >= 0 && l2 >= 0 && k2 < 108 && l2 < 19) {
-                    gfx.blit(TEXTURE, slotsX, yCenter + 14 + 19 * slot, 0, 166 + 38, 108, 19);
-                    color = 0xFFFF80;
-                }
-                else {
-                    gfx.blit(TEXTURE, slotsX, yCenter + 14 + 19 * slot, 0, 166, 108, 19);
-                    color = 0xCDCDCD;
-                }
-                gfx.blit(TEXTURE, slotsX + 1, yCenter + 15 + 19 * slot, 16 * slot, 223, 16, 16);
-
-                gfx.drawWordWrap(this.font, randText, randTextX, yCenter + 16 + 19 * slot, width, color);
-                color = rarity.getColor().getValue();
-            }
-            this.drawBorderedString(gfx, costStr, slotsX + 10, yCenter + 21 + 19 * slot, color, this.darken(color, 4));
-            this.drawBorderedString(gfx, levelStr, slotsX + 106 - this.font.width(levelStr), yCenter + 16 + 19 * slot + 7, color, this.darken(color, 4));
+        if (this.hasMainItem) {
+            // 127x112 12 frames at 24fps
+            // pos 26,15
+            float delta = Mth.clamp((FRAME_TIME - this.animationTick - partials) / FRAME_TIME, 0, 1);
+            int frame = Mth.lerpInt(delta, 0, 20);
+            gfx.blit(ANIMATED_TEXTURE, left + 26, top + 15, 127, 112, 0, frame * 112, 127, 112, 127, 2240);
         }
     }
 
@@ -191,12 +133,6 @@ public class ReforgingScreen extends AbstractContainerScreen<ReforgingMenu> impl
     }
 
     @Override
-    protected void renderLabels(GuiGraphics gfx, int x, int y) {
-        gfx.drawString(this.font, this.title, this.titleLabelX, this.titleLabelY, 4210752, false);
-        gfx.drawString(this.font, this.playerInventoryTitle, this.inventoryLabelX, this.inventoryLabelY, 4210752, false);
-    }
-
-    @Override
     public boolean mouseClicked(double pMouseX, double pMouseY, int pButton) {
         int i = (this.width - this.imageWidth) / 2;
         int j = (this.height - this.imageHeight) / 2;
@@ -211,6 +147,50 @@ public class ReforgingScreen extends AbstractContainerScreen<ReforgingMenu> impl
         }
 
         return super.mouseClicked(pMouseX, pMouseY, pButton);
+    }
+
+    @Override
+    protected void containerTick() {
+        int dust = this.menu.getDustCount();
+        int mats = this.menu.getMatCount();
+        int levels = this.menu.player.experienceLevel;
+
+        this.maxSlot = -1;
+
+        for (int idx = 0; idx < 3; idx++) {
+            Slot slot = this.getMenu().getSlot(3 + idx);
+            if (!slot.hasItem()) break;
+
+            int dustCost = this.menu.getDustCost(idx);
+            int matCost = this.menu.getMatCost(idx);
+            int levelCost = this.menu.getLevelCost(idx);
+
+            if ((dust >= dustCost && levels >= levelCost && mats >= matCost) || this.minecraft.player.getAbilities().instabuild) {
+                this.maxSlot++;
+            }
+        }
+
+        int ticks = this.minecraft.player.tickCount % 60;
+        float sin = Mth.sin((ticks / 60F) * Mth.PI);
+        float delta = sin * sin;
+        this.availableOpacity = Mth.lerpInt(delta, 0x88, 0xDD);
+
+        if (this.animationTick > 0) {
+            this.animationTick--;
+        }
+    }
+
+    @Override
+    public void renderSlot(GuiGraphics pGuiGraphics, Slot pSlot) {
+        if (pSlot instanceof ReforgingResultSlot) {
+            if (this.animationTick == 0) {
+                int opacity = this.maxSlot >= pSlot.getContainerSlot() ? this.availableOpacity : 0x40;
+                SalvagingScreen.renderGuiItem(pGuiGraphics, pSlot.getItem(), pSlot.x, pSlot.y, GhostVertexBuilder.makeGhostBuffer(opacity));
+            }
+        }
+        else {
+            super.renderSlot(pGuiGraphics, pSlot);
+        }
     }
 
 }
