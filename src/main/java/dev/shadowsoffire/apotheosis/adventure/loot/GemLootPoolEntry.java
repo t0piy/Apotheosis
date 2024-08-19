32d75af587bee266fc69845f9ac41acd4eee0780
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2024-08-18 20:34:51 -0700

    Add explicit rarity clamp support to GemLootPoolEntry

--- src/main/java/dev/shadowsoffire/apotheosis/adventure/loot/GemLootPoolEntry.java
+++ src/main/java/dev/shadowsoffire/apotheosis/adventure/loot/GemLootPoolEntry.java
@@ -36,10 +36,13 @@ public class GemLootPoolEntry extends LootPoolSingletonContainer {
     public static final Serializer SERIALIZER = new Serializer();
     public static final LootPoolEntryType TYPE = new LootPoolEntryType(SERIALIZER);
 
+    @Nullable
+    private final RarityClamp.Simple rarityLimit;
     private final List<DynamicHolder<Gem>> gems;
 
-    public GemLootPoolEntry(List<ResourceLocation> gems, int weight, int quality, LootItemCondition[] conditions, LootItemFunction[] functions) {
+    public GemLootPoolEntry(@Nullable RarityClamp.Simple rarityLimit, List<ResourceLocation> gems, int weight, int quality, LootItemCondition[] conditions, LootItemFunction[] functions) {
         super(weight, quality, conditions, functions);
+        this.rarityLimit = rarityLimit;
         this.gems = gems.stream().map(GemRegistry.INSTANCE::holder).toList();
     }
 
@@ -57,7 +60,7 @@ public class GemLootPoolEntry extends LootPoolSingletonContainer {
             gem = GemRegistry.INSTANCE.getRandomItem(ctx.getRandom(), ctx.getLuck(), IDimensional.matches(ctx.getLevel()), IStaged.matches(player));
         }
 
-        RarityClamp clamp = AdventureConfig.GEM_DIM_RARITIES.get(ctx.getLevel().dimension().location());
+        RarityClamp clamp = this.rarityLimit == null ? AdventureConfig.GEM_DIM_RARITIES.get(ctx.getLevel().dimension().location()) : this.rarityLimit;
         ItemStack stack = GemRegistry.createGemStack(gem, gem.clamp(LootRarity.random(ctx.getRandom(), ctx.getLuck(), clamp)));
         list.accept(stack);
     }
@@ -90,13 +93,26 @@ public class GemLootPoolEntry extends LootPoolSingletonContainer {
     public static class Serializer extends LootPoolSingletonContainer.Serializer<GemLootPoolEntry> {
 
         @Override
-        protected GemLootPoolEntry deserialize(JsonObject jsonObject, JsonDeserializationContext context, int weight, int quality, LootItemCondition[] lootConditions, LootItemFunction[] lootFunctions) {
-            List<String> gems = context.deserialize(GsonHelper.getAsJsonArray(jsonObject, "gems", new JsonArray()), new TypeToken<List<String>>(){}.getType());
-            return new GemLootPoolEntry(gems.stream().map(ResourceLocation::new).toList(), weight, quality, lootConditions, lootFunctions);
+        protected GemLootPoolEntry deserialize(JsonObject obj, JsonDeserializationContext context, int weight, int quality, LootItemCondition[] lootConditions, LootItemFunction[] lootFunctions) {
+            RarityClamp.Simple rarity;
+            if (obj.has("min_rarity") || obj.has("max_rarity")) {
+                DynamicHolder<LootRarity> minRarity = RarityRegistry.byLegacyId(GsonHelper.getAsString(obj, "min_rarity"));
+                DynamicHolder<LootRarity> maxRarity = RarityRegistry.byLegacyId(GsonHelper.getAsString(obj, "max_rarity"));
+                rarity = new RarityClamp.Simple(minRarity, maxRarity);
+            }
+            else {
+                rarity = null;
+            }
+            List<String> gems = context.deserialize(GsonHelper.getAsJsonArray(obj, "gems", new JsonArray()), new TypeToken<List<String>>(){}.getType());
+            return new GemLootPoolEntry(rarity, gems.stream().map(ResourceLocation::new).toList(), weight, quality, lootConditions, lootFunctions);
         }
 
         @Override
         public void serializeCustom(JsonObject object, GemLootPoolEntry e, JsonSerializationContext ctx) {
+            if (e.rarityLimit != null) {
+                object.addProperty("min_rarity", e.rarityLimit.min().getId().toString());
+                object.addProperty("max_rarity", e.rarityLimit.max().getId().toString());
+            }
             object.add("gems", ctx.serialize(e.gems));
             super.serializeCustom(object, e, ctx);
         }
